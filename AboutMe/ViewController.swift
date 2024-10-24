95329799b356dc0110ea33a536141147dc86851b
Norman Sánchez <normansanchezn@gmail.com>
2024-10-24 16:46:52 -0600

    Initial Commit

---
+++ AboutMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by Norman Sanchez on 24/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AboutMeTests/AboutMeTests.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeTests.swift
+//  AboutMeTests
+//
+//  Created by Norman Sanchez on 24/10/24.
+//
+
+import Testing
+@testable import AboutMe
+
+struct AboutMeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
